cd9a6ebe93c4d994ca530f8b19708dcc69e41f0c
常行 <changxing.lj@antfin.com>
2020-12-17 22:07:53 +0800

    refactor: format

--- src/ImageData.ts
+++ src/ImageData.ts
@@ -1,36 +1,36 @@
 export class ImageData {
-    private _w: number;
-    private _h: number;
-    private _data: Uint8ClampedArray;
+  private _w: number;
+  private _h: number;
+  private _data: Uint8ClampedArray;
 
-    constructor() {
-        let len = arguments.length;
-        if (len == 2) {
-            if (typeof arguments[0] == 'number' && typeof arguments[1] == 'number') {
-                this._w = arguments[0];
-                this._h = arguments[1];
-                this._data = new Uint8ClampedArray(this._w * this._h * 4);
-                return;
-            }
-        } else if (len == 3) {
-            if (typeof arguments[0] == 'object' && typeof arguments[1] == 'number' && typeof arguments[2] == 'number') {
-                this._data = arguments[0];
-                this._w = arguments[1];
-                this._h = arguments[2];
-            }
-        }
-        throw new Error('ImageData: params error');
+  constructor() {
+    let len = arguments.length;
+    if (len == 2) {
+      if (typeof arguments[0] == "number" && typeof arguments[1] == "number") {
+        this._w = arguments[0];
+        this._h = arguments[1];
+        this._data = new Uint8ClampedArray(this._w * this._h * 4);
+        return;
+      }
+    } else if (len == 3) {
+      if (typeof arguments[0] == "object" && typeof arguments[1] == "number" && typeof arguments[2] == "number") {
+        this._data = arguments[0];
+        this._w = arguments[1];
+        this._h = arguments[2];
+      }
     }
+    throw new Error("ImageData: params error");
+  }
 
-    get width(): number {
-        return this._w;
-    }
+  get width(): number {
+    return this._w;
+  }
 
-    get height(): number {
-        return this._h;
-    }
+  get height(): number {
+    return this._h;
+  }
 
-    get data(): Uint8ClampedArray {
-        return this._data;
-    }
+  get data(): Uint8ClampedArray {
+    return this._data;
+  }
 }
