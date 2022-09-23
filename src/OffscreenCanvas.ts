5a49689f013a83c768bdd3c995ab44b9ab88d3bb
AZhan <njktsmshh@163.com>
2022-09-23 17:08:46 +0800

    feat: adapter MiniAdapter (#13)

--- src/OffscreenCanvas.ts
+++ src/OffscreenCanvas.ts
@@ -1,1 +1,16 @@
-export class OffscreenCanvas extends Object {}
+import { isMiniGame } from "./register";
+declare let my: any;
+
+export class OffscreenCanvas {
+  constructor(width: number = 0, height: number = 0) {
+    let canvas;
+    if (isMiniGame()) {
+      canvas = my.createCanvas();
+      canvas.width = width;
+      canvas.height = height;
+    } else {
+      canvas = my.createOffscreenCanvas(width, height);
+    }
+    return canvas;
+  }
+}
