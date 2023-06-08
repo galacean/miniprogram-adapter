6b6e8774853af3bd69d184a8d03390d4ea6a3f3c
cptbtptpbcptdtptp <njktsmshh@163.com>
2023-06-08 14:53:26 +0800

    feat: adapter performance

--- src/performance.ts
+++ src/performance.ts
@@ -1,2 +1,19 @@
-// 暂不支持
-export const performance = null;
+export class performance {
+  static readonly eventCounts: number = 0;
+  static readonly timeOrigin: number = 0;
+
+  static now() {
+    return Date.now();
+  }
+
+  static clearMarks() {}
+  static clearMeasures() {}
+  static clearResourceTimings() {}
+  static getEntries() {}
+  static getEntriesByName() {}
+  static getEntriesByType() {}
+  static mark() {}
+  static measure() {}
+  static setResourceTimingBufferSize() {}
+  static toJSON() {}
+}
